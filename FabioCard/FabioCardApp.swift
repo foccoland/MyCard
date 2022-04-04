5a4d97541227833075f49c8120dd2e305451694b
Fabio Giannelli <fabio.giannelli@eng.it>
2022-04-04 14:20:31 +0200

    Initial Commit

---
+++ FabioCard/FabioCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  FabioCardApp.swift
+//  FabioCard
+//
+//  Created by Fabio Giannelli on 04/04/22.
+//
+
+import SwiftUI
+
+@main
+struct FabioCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
